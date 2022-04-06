f27fdbaad3e7432817ac23a8d2c67e438a337a0e
Olga <ngsolya@gmail.com>
2022-04-06 15:45:47 +0600

    выполнено задние 02 Age, не выводит русский язык

--- 02_unit/Age/src/Main.java
+++ 02_unit/Age/src/Main.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Main {
-}
+public class Main {
+    public static void main(String[] args) {
+//Напишите программу, которая выводит возрасты трёх людей в порядке увеличения
+        int petyaAge = 6;
+        int lenaAge = 90;
+        int romaAge = 6;
+
+        int min = -1;
+        int middle = -1;
+        int max = -1;
+
+        if (petyaAge >= lenaAge && petyaAge >= romaAge){
+            max = petyaAge;
+            if ( lenaAge >= romaAge) {
+                middle = lenaAge;
+                min = romaAge;
+            }
+            else {
+                middle = romaAge;
+                min = lenaAge;
+            }
+            }
+
+        if (lenaAge >= petyaAge && lenaAge >= romaAge){
+            max = lenaAge;
+            if ( petyaAge >= romaAge){
+                middle = petyaAge;
+                min = romaAge;
+            }
+            else {
+                middle = romaAge;
+                min = petyaAge;
+            }
+        }
+
+        if (romaAge >= lenaAge && romaAge >= petyaAge){
+            max = romaAge;
+            if ( lenaAge >= petyaAge){
+                middle = lenaAge;
+                min = petyaAge;
+            }
+            else {
+                middle = petyaAge;
+                min = lenaAge;
+            }
+        }
+        System.out.println( "Minimal age: " + min);
+        System.out.println( "Middle age: " + middle);
+        System.out.println( "Максимальный возраст: " + max);
+        }
+    }
+
--- 02_unit/Booleans/src/Main.java
+++ 02_unit/Booleans/src/Main.java
@@ -1,26 +1,36 @@
 
 public class Main {
     public static void main(String[] args) {
-        int milkAmount = 200; // ml
-        int powderAmount = 5; // g
-        int eggsCount = 3; // items
-        int sugarAmount = 5; // g
+
+        int milkAmount = 2000; // ml
+        int powderAmount = 600; // g
+        int eggsCount = 9; // items
+        int sugarAmount = 15; // g
         int oilAmount = 30; // ml
         int appleCount = 8;
 
         // Example
         // appleCount - 5
+        {
+            System.out.println("Cooking:");
+        }
         if (appleCount > 5 ) {
             System.out.println("Apple Juice");
         }
 
         //powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
-        System.out.println("Pancakes");
+        if ( powderAmount >= 400 && sugarAmount >= 10 && milkAmount >= 1000 && oilAmount >= 30 ) {
+            System.out.println("Pancakes");
+        }
 
         //milk - 300 ml, powder - 5 g, eggs - 5
-        System.out.println("Omelette");
+        if ( milkAmount >= 300 && powderAmount >= 5 && eggsCount >= 5 ) {
+            System.out.println("Omelette");
+        }
 
         //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
-        System.out.println("Apple pie");
+        if (appleCount >= 3 && milkAmount >= 100 && eggsCount >= 4 ) {
+            System.out.println("Apple pie");
+        }
     }
 }
