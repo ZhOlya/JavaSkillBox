228efb244a9aef8ac15771d7f4752d176b039ea2
Olga <ngsolya@gmail.com>
2022-06-02 22:27:58 +0600

    CALCULATOR method is not called

---
+++ 06_unit/ArithmeticCalculator/src/ArithmeticCalculator.java
@@ -0,0 +1,30 @@
+public class ArithmeticCalculator {
+    public int x;
+    public int y;
+
+    public ArithmeticCalculator(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void calculate (Operation operation){
+        switch (operation){
+            case ADD -> System.out.println("Sum: " + x + y);
+            case SUBTRACT -> System.out.println("Difference: " + (x - y));
+            case MULTIPLY -> System.out.println("Product: " + x * y);
+        }
+
+    }
+}
+/*
+—оздайте класс ArithmeticCalculator, который будет выполн€ть арифметические операции над числами.
+
+–еализуйте конструктор с двум€ параметрами-числами, который сохран€ет переданные числа в свойствах класса.
+
+–еализуйте метод calculate, на вход которого передаЄтс€ одна из арифметических операций (объект
+ класса Operation Ч смотрите следующий пункт задани€), которую нужно произвести с двум€ числами,
+  переданными в конструктор при создании объекта.
+
+—оздайте Enum Operation с трем€ значени€ми: ADD, SUBTRACT, MULTIPLY. Ёти значени€ будут
+соответствовать арифметическим операци€м Ч сложению, вычитанию и умножению.
+ */
---
+++ 06_unit/ArithmeticCalculator/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    ArithmeticCalculator one = new ArithmeticCalculator(5,7);
+    one.ca
+
+
+}
