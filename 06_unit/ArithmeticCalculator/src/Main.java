c037de8aba2453e6f39c8e3dcb2e982fb6ec4bae
Olga <ngsolya@gmail.com>
2022-06-04 18:22:08 +0600

    calculator is done part 2

--- 06_unit/ArithmeticCalculator/src/Main.java
+++ 06_unit/ArithmeticCalculator/src/Main.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        ArithmeticCalculator one = new ArithmeticCalculator(5,7);
-        one.calculate(Operation.ADD);
-        one.calculate(Operation.SUBTRACT);
-        one.calculate(Operation.MULTIPLY);
+       ArithmeticCalculator one = new ArithmeticCalculator();
+
+       while (true) {
+           System.out.println("please, enter the two numbers ");
+
+           int z = new Scanner(System.in).nextInt();
+           int v = new Scanner(System.in).nextInt();
+
+           one.setX(z);
+           one.setY(v);
+
+           one.calculate(Operation.ADD);
+           one.calculate(Operation.SUBTRACT);
+           one.calculate(Operation.MULTIPLY);
+       }
     }
-
-
-
-
 }
