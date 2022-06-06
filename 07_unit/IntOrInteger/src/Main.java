7aa10edf34e2a23b23ab0845cd79881a5e3cb556
Olga <ngsolya@gmail.com>
2022-06-06 22:55:46 +0600

    Alphabet of russian language with symbol "Ёё"

---
+++ 07_unit/IntOrInteger/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        Container container = new Container();
+        container.addCount(5672);
+        System.out.println(container.getCount());
+
+        // TODO: ниже напишите код для выполнения задания:
+        //  С помощью цикла и преобразования чисел в символы найдите все коды
+        //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
+
+
+        for(char symbol = 'А'; symbol <= 'я'; symbol ++){
+            int numb = symbol;
+            System.out.println(symbol + " - " + numb);
+        }
+        System.out.println("Ё" + " - " +(int)'Ё');
+        System.out.println("ё" + " - " + (int)'ё');
+
+    }
+}
